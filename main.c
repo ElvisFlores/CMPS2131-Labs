396fd03b7215991b17db8050a93b08e68249f28d
Elvis Flores <2024161113@ub.edu.bz>
2026-08-22 13:29:30 -0600

    commit

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 #include<stddef.h>
 #include "stats.h"
+#include"record.h"
+#include <stdlib.h> 
+#include "node.h"
 
 int main(void){ 
+    // part 6
     const int numbers[] = {  10 , 20 , 30 ,40 ,50};
 
     size_t length = sizeof(numbers) / sizeof(numbers[0]);
@@ -11,5 +15,56 @@ int main(void){
 
     printf("The sum of the array is : %d\n", total_sum);
 
-    return 0;
+    //part 7 
+
+    Record item = {.id = 101 , .value = 45.75};
+
+    printf("\n Original fields (using .): \n");
+    printf("ID : %d\n", item.id);
+    printf("Value: %.2f\n", item.value);
+
+    Record *record_ptr =&item;
+
+    printf("\n Fields through pointers (using ->) : \n");
+    printf("ID: %d\n " , record_ptr ->id);
+    printf("Value: %.2f\n", record_ptr->value);
+
+    record_ptr ->value = 99.99;
+
+    printf("\nVerified original object (after pointer modification):\n");
+    printf("ID: %d\n", item.id);
+    printf("Value: %.2f\n", item.value);
+
+    // part 8 
+     Record *record = malloc(sizeof *record);
+ 
+    if (record == nullptr) {
+        fputs("memory allocation failed\n", stderr);
+        return EXIT_FAILURE;
+    }
+ 
+    record->id = 1;
+    record->value = 42.5;
+ 
+    printf("\nDynamic Memory Allocation:\n");
+    printf("id=%d value=%.1f\n", record->id, record->value);
+ 
+    free(record);
+    //part 9 
+  Node *node = malloc(sizeof *node);
+
+    if (node == nullptr) {
+        fputs("node allocation failed\n", stderr);
+        return EXIT_FAILURE;
+    }
+
+    node->value = 100;
+    node->next = nullptr;
+
+    printf("\nNode Allocation:\n");
+    printf("value=%d\n", node->value);
+
+    free(node);
+
+    return EXIT_SUCCESS;
 }
